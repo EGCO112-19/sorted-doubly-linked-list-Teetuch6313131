a126596b061cfea6b962f2a50d5336ce50e5c366
Teetuch6313131 <teetuch.sir@student.mahidol.ac.th>
2021-02-19 06:51:30 +0000

    change linklist

--- linkedlist.h
+++ linkedlist.h
@@ -3,7 +3,10 @@ struct Node {
    int data; // each listNode contains a character 
    struct Node *nextPtr; // pointer to next node
 }; // end structure listNode                        
+typedef struct Node LLnode; // synonym for struct listNode
+typedef LLnode *LLPtr; // synonym for ListNode*
 // prototypes
+
 
 int deletes( LLPtr *sPtr, int value );
 int isEmpty( LLPtr sPtr );
@@ -11,8 +14,6 @@ void insert( LLPtr *sPtr, int value );
 void printList( LLPtr currentPtr );
 void instructions( void );
 
-typedef struct Node LLnode; // synonym for struct listNode
-typedef LLnode *LLPtr; // synonym for ListNode*
 
 
 // display program instructions to user
